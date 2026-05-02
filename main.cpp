56c4937c813e12d17715985e86bb0f64118d1dce
Adriana Kisil <284559@student.pwr.edu.pl>
2026-05-02 12:14:07 +0200

    Clean up main test flow

--- main.cpp
+++ main.cpp
@@ -3,17 +3,30 @@
 #include "ListGraph.h"
 #include "FileLoader.h"
 
+// Wyswietlenie podstawowej instrukcji programu
+void showHelp() {
+    std::cout << "AiZO Projekt 2 - grafy\n";
+    std::cout << "Tryby programu zostana dodane w kolejnym etapie.\n\n";
+
+    std::cout << "Aktualny test:\n";
+    std::cout << " - wczytanie grafu z pliku\n";
+    std::cout << " - wyswietlenie grafu w macierzy\n";
+    std::cout << " - wyswietlenie grafu w liscie\n";
+}
+
 int main() {
-    // Tworzenie grafów w dwóch reprezentacjach
-    MatrixGraph graph(4);
+    showHelp();
+
+    // Tworzenie grafow w dwoch reprezentacjach
+    MatrixGraph matrixGraph(4);
     ListGraph listGraph(4);
 
-    // Wczytywanie grafu z pliku
-    FileLoader::loadMatrixGraph("../data/graph.txt", graph);
+    // Wczytywanie grafu z pliku testowego
+    FileLoader::loadMatrixGraph("../data/graph.txt", matrixGraph);
     FileLoader::loadListGraph("../data/graph.txt", listGraph);
 
     std::cout << "\nMacierz grafu:\n";
-    graph.print();
+    matrixGraph.print();
 
     std::cout << "\nLista grafu:\n";
     listGraph.print();
