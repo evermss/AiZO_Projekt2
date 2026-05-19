c1a7f54fdc7685b8f14d2b09d3bc3031666e53f6
Adriana Kisil <284559@student.pwr.edu.pl>
2026-05-19 23:16:31 +0200

    Add single test mode and graph loading

--- main.cpp
+++ main.cpp
@@ -7,39 +7,55 @@
 // Wyswietlenie podstawowej instrukcji programu
 void showHelp() {
     std::cout << "AiZO Projekt 2 - grafy\n";
-    std::cout << "Tryby programu zostana dodane w kolejnym etapie.\n\n";
-
-    std::cout << "Aktualny test:\n";
-    std::cout << " - wczytanie grafu z pliku\n";
-    std::cout << " - wyswietlenie grafu w macierzy\n";
-    std::cout << " - wyswietlenie grafu w liscie\n";
+    std::cout << "Tryby programu:\n";
+    std::cout << " - help       -> pomoc\n";
+    std::cout << " - singleFile -> pojedynczy test\n";
+    std::cout << " - benchmark  -> badania\n";
 }
 
-int main(int argc, char* argv[]) {
-
-    if (argc > 1) {
-        Parameters::readParameters(argc, argv);
-    }
-
-    // Tryb pomocy
-    if (Parameters::runMode == Parameters::RunModes::help) {
-        Parameters::help();
-        return 0;
-    }
-
-    showHelp();
-
+// Tryb pojedynczego testu
+void runSingleTest() {
     MatrixGraph matrixGraph(4);
     ListGraph listGraph(4);
 
-    FileLoader::loadMatrixGraph("graph.txt", matrixGraph);
-    FileLoader::loadListGraph("graph.txt", listGraph);
+    FileLoader::loadMatrixGraph("data/graph.txt", matrixGraph);
+    FileLoader::loadListGraph("data/graph.txt", listGraph);
 
     std::cout << "\nMacierz grafu:\n";
     matrixGraph.print();
 
     std::cout << "\nLista grafu:\n";
     listGraph.print();
+}
+
+int main(int argc, char* argv[]) {
+    // Bez argumentow uruchamiamy prosty test
+    if (argc == 1) {
+        showHelp();
+        runSingleTest();
+        return 0;
+    }
+
+    // Odczyt parametrow z biblioteki prowadzacego
+    Parameters::readParameters(argc, argv);
+
+    if (Parameters::runMode == Parameters::RunModes::help) {
+        Parameters::help();
+        return 0;
+    }
+
+    if (Parameters::runMode == Parameters::RunModes::singleFile) {
+        runSingleTest();
+        return 0;
+    }
+
+    if (Parameters::runMode == Parameters::RunModes::benchmark) {
+        std::cout << "Tryb badan zostanie dodany w kolejnym etapie.\n";
+        return 0;
+    }
+
+    std::cout << "Nieznany tryb programu.\n";
+    showHelp();
 
     return 0;
 }
