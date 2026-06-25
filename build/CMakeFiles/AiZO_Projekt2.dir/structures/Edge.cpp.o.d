612c27127b83fe0fcbbe5e70f6a5717dd89712ef
Adriana Kisil <284559@student.pwr.edu.pl>
2026-06-25 18:16:25 +0200

    Clean repository before submission

--- build/CMakeFiles/AiZO_Projekt2.dir/structures/Edge.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/AiZO_Projekt2.dir/structures/Edge.cpp.o: \
- /home/adriana/284559_Projekt2/AiZO_Projekt2/structures/Edge.cpp \
- /usr/include/stdc-predef.h \
- /home/adriana/284559_Projekt2/AiZO_Projekt2/structures/Edge.h
