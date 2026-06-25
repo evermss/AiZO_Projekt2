c1f74414d34e182e9c00e909f2f391650da20b13
Adriana Kisil <284559@student.pwr.edu.pl>
2026-06-25 13:57:19 +0200

    Fix Prim MST for undirected graph

--- algorithms/Prim.cpp
+++ algorithms/Prim.cpp
@@ -1,6 +1,49 @@
 #include "Prim.h"
 #include <iostream>
 
+static const int INF = 999999;
+
+static int getUndirectedMatrixWeight(MatrixGraph& graph, int u, int v) {
+    int weightUV = graph.getEdge(u, v);
+    int weightVU = graph.getEdge(v, u);
+
+    if (weightUV > 0 && weightVU > 0) {
+        return weightUV < weightVU ? weightUV : weightVU;
+    }
+
+    if (weightUV > 0) {
+        return weightUV;
+    }
+
+    return weightVU;
+}
+
+static void printPrimResult(int* parent, int* key, int vertices) {
+    std::cout << "MST Prim:\n";
+
+    int totalWeight = 0;
+    bool connected = true;
+
+    for (int i = 1; i < vertices; i++) {
+        if (parent[i] == -1 || key[i] == INF) {
+            connected = false;
+            break;
+        }
+    }
+
+    if (!connected) {
+        std::cout << "Graf jest niespojny - nie mozna wyznaczyc MST dla wszystkich wierzcholkow.\n";
+        return;
+    }
+
+    for (int i = 1; i < vertices; i++) {
+        std::cout << parent[i] << " - " << i << "  waga: " << key[i] << "\n";
+        totalWeight += key[i];
+    }
+
+    std::cout << "Suma wag: " << totalWeight << "\n";
+}
+
 void Prim::runMatrix(MatrixGraph& graph) {
     int vertices = graph.getVertices();
 
@@ -10,14 +53,14 @@ void Prim::runMatrix(MatrixGraph& graph) {
 
     for (int i = 0; i < vertices; i++) {
         visited[i] = false;
-        key[i] = 999999;
+        key[i] = INF;
         parent[i] = -1;
     }
 
     key[0] = 0;
 
     for (int count = 0; count < vertices - 1; count++) {
-        int min = 999999;
+        int min = INF;
         int u = -1;
 
         for (int v = 0; v < vertices; v++) {
@@ -27,10 +70,14 @@ void Prim::runMatrix(MatrixGraph& graph) {
             }
         }
 
+        if (u == -1) {
+            break;
+        }
+
         visited[u] = true;
 
         for (int v = 0; v < vertices; v++) {
-            int weight = graph.getEdge(u, v);
+            int weight = getUndirectedMatrixWeight(graph, u, v);
 
             if (weight > 0 && !visited[v] && weight < key[v]) {
                 parent[v] = u;
@@ -39,16 +86,7 @@ void Prim::runMatrix(MatrixGraph& graph) {
         }
     }
 
-    std::cout << "MST Prim:\n";
-
-    int totalWeight = 0;
-
-    for (int i = 1; i < vertices; i++) {
-        std::cout << parent[i] << " - " << i << "  waga: " << key[i] << "\n";
-        totalWeight += key[i];
-    }
-
-    std::cout << "Suma wag: " << totalWeight << "\n";
+    printPrimResult(parent, key, vertices);
 
     delete[] visited;
     delete[] key;
@@ -64,14 +102,14 @@ void Prim::runList(ListGraph& graph) {
 
     for (int i = 0; i < vertices; i++) {
         visited[i] = false;
-        key[i] = 999999;
+        key[i] = INF;
         parent[i] = -1;
     }
 
     key[0] = 0;
 
     for (int count = 0; count < vertices - 1; count++) {
-        int min = 999999;
+        int min = INF;
         int u = -1;
 
         for (int v = 0; v < vertices; v++) {
@@ -81,33 +119,40 @@ void Prim::runList(ListGraph& graph) {
             }
         }
 
+        if (u == -1) {
+            break;
+        }
+
         visited[u] = true;
 
-        LinkedList<Edge>& list = graph.getAdjList(u);
+        // MST jest dla grafu nieskierowanego, dlatego dla listy sasiedztwa
+        // sprawdzamy krawedzie wychodzace z u oraz krawedzie dochodzace do u.
+        for (int i = 0; i < vertices; i++) {
+            LinkedList<Edge>& list = graph.getAdjList(i);
 
-        for (int i = 0; i < list.getSize(); i++) {
-            Edge edge = list.get(i);
+            for (int j = 0; j < list.getSize(); j++) {
+                Edge edge = list.get(j);
 
-            int v = edge.getEnd();
-            int weight = edge.getWeight();
+                int edgeStart = edge.getStart();
+                int edgeEnd = edge.getEnd();
+                int weight = edge.getWeight();
+                int v = -1;
 
-            if (!visited[v] && weight < key[v]) {
-                parent[v] = u;
-                key[v] = weight;
+                if (edgeStart == u) {
+                    v = edgeEnd;
+                } else if (edgeEnd == u) {
+                    v = edgeStart;
+                }
+
+                if (v != -1 && !visited[v] && weight < key[v]) {
+                    parent[v] = u;
+                    key[v] = weight;
+                }
             }
         }
     }
 
-    std::cout << "MST Prim:\n";
-
-    int totalWeight = 0;
-
-    for (int i = 1; i < vertices; i++) {
-        std::cout << parent[i] << " - " << i << "  waga: " << key[i] << "\n";
-        totalWeight += key[i];
-    }
-
-    std::cout << "Suma wag: " << totalWeight << "\n";
+    printPrimResult(parent, key, vertices);
 
     delete[] visited;
     delete[] key;
